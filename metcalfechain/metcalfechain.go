7caf5e6cf272975a32734586e7a51d5e0d4b81fd
heshuchao <heshuchao@hotmail.com>
2020-10-17 15:34:26 +0800

    fix bug

--- metcalfechain/metcalfechain.go
+++ metcalfechain/metcalfechain.go
@@ -106,7 +106,7 @@ func (wm *WalletManager) Decimal() int32 {
 }
 
 //AddressDecode 地址解析器
-func (wm *WalletManager) GetAddressDecode() openwallet.AddressDecoder {
+func (wm *WalletManager) GetAddressDecoderV2() openwallet.AddressDecoderV2 {
 	return wm.Decoder
 }
 
